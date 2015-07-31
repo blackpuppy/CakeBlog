73ba3f2ee4275c5843c84bcc37fe5fd9ac362d3f
Zhu Ming <mingzhu.z+github@gmail.com>
2015-07-31 15:43:52 +0800

    add demo for CookieComponenet
    - work for non-AJAX demo

--- src/blog/app/Controller/CookieDemoController.php
+++ src/blog/app/Controller/CookieDemoController.php
@@ -12,8 +12,8 @@ class CookieDemoController extends AppController {
 	    $this->Cookie->name = 'cookie_demo';
 	    $this->Cookie->time = '1 hour';
 	    $this->Cookie->path = '/cookie_demo/';
-	    $this->Cookie->domain = '.cakeblog.dev';
-	    $this->Cookie->secure = true;  // i.e. only sent if using secure HTTPS
+	    $this->Cookie->domain = 'cakeblog.dev';
+	    // $this->Cookie->secure = true;  // i.e. only sent if using secure HTTPS
 	    $this->Cookie->key = 'vAl80DrsEeoEfe90y~=L8E9`w-\8^#088|a)8)j[&3(2o*38i&1I*(`{H';
 	    $this->Cookie->httpOnly = true;
 	}
@@ -38,13 +38,15 @@ class CookieDemoController extends AppController {
 		$msg = PHP_EOL . 'CookieDemoController::read_cookie():'
 			. PHP_EOL . '  $this->request->url = ' . $this->request->url;
 
+	    // $this->Cookie->name = 'cookie_demo';
+		$cookie = $this->Cookie->read();
     	$time = $this->Cookie->read('simple_cookie');
     	$complex = $this->Cookie->read('complex_cookie');
 
 		$msg .= PHP_EOL . '  $time = ' . $time
 			. PHP_EOL . '  $complex = ' . Debugger::exportVar($complex);
 
-    	$this->set(compact('time', 'complex'));
+    	$this->set(compact('cookie', 'time', 'complex'));
 
 		$msg .= PHP_EOL . str_repeat('-', 80);
 		CakeLog::write(LOG_DEBUG, $msg);
--- src/blog/app/View/CookieDemo/read_cookie.ctp
+++ src/blog/app/View/CookieDemo/read_cookie.ctp
@@ -17,6 +17,8 @@
 
 <h2>Cookie Values:</h2>
 <dl>
+    <dt>Cookie</dt>
+    <dd><?php echo Debugger::exportVar($cookie, 5); ?></dd>
     <dt>Simple</dt>
     <dd><?php echo $time; ?></dd>
     <dt>complex.location</dt>
