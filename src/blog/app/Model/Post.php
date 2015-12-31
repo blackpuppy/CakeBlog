2b4bc1c0e7060f73cc3ff98340c2eef64350affa
Zhu Ming <mingzhu.z+gitlab@gmail.com>
2015-12-31 23:59:55 +0800

    update for breaking changes in CakePHP 2.7

--- src/blog/app/Model/Post.php
+++ src/blog/app/Model/Post.php
@@ -3,18 +3,18 @@
 App::uses('Model', 'Model');
 
 class Post extends AppModel {
-    public $validate = array(
-        'title' => array(
-            'rule' => 'notEmpty'
-        ),
-        'body' => array(
-            'rule' => 'notEmpty'
-        )
-    );
+	public $validate = array(
+		'title' => array(
+			'rule' => 'notBlank'
+		),
+		'body' => array(
+			'rule' => 'notBlank'
+		)
+	);
 
-    public $hasAndBelongsToMany = array(
-    	'Tag' => array(
-    		'className' => 'Tag'
+	public $hasAndBelongsToMany = array(
+		'Tag' => array(
+			'className' => 'Tag'
 			,'joinTable' => 'posts_tags'
 			,'foreignKey' => 'post_id'
 			,'associationForeignKey' => 'tag_id'
--- src/blog/app/Model/Tag.php
+++ src/blog/app/Model/Tag.php
@@ -3,15 +3,15 @@
 App::uses('Model', 'Model');
 
 class Tag extends AppModel {
-    public $validate = array(
-        'name' => array(
-            'rule' => 'notEmpty'
-        )
-    );
+	public $validate = array(
+		'name' => array(
+			'rule' => 'notBlank'
+		)
+	);
 
-    public $hasAndBelongsToMany = array(
-    	'Post' => array(
-    		'className' => 'Post'
+	public $hasAndBelongsToMany = array(
+		'Post' => array(
+			'className' => 'Post'
 			,'joinTable' => 'posts_tags'
 			,'foreignKey' => 'tag_id'
 			,'associationForeignKey' => 'post_id'
